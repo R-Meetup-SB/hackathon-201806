14eafb3edb29caec94a344f9596e7e51880d2dbf
raviolli77 <rauleulogio3@gmail.com>
2018-07-01 23:29:18 -0700

    final edits to data cleaning script

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,6 +1,6 @@
 # Ideally this would be replaced with creating a `.Rproject` file 
 # used in conjunction with the `here` package 
-# setwd('C:\\Users\\reulogio\\Desktop\\throwaway\\hackathon')
+# setwd('/some/location/on/your/computer')
 
 # Created By: Raul Eulogio
 
@@ -32,6 +32,8 @@ file_path <- here::here("hackathon_20180621.xlsx")
 
 # Load Data ---------------------------------------------------------------
 read_sheets_max <- function(x, path_name){
+  # Custom function which includes the set parameter `guess_max` to be 2000
+  # Utilized due to columns being read in as logical data types incorrectly
   excel_sheet <- readxl::read_xlsx(x, path = path_name, guess_max = 2000)
 }
 
@@ -41,87 +43,97 @@ my_data <- file_path %>%
   purrr::set_names() %>%
   purrr::map(read_sheets_max, path = file_path)
 
-# Create dataframes for each sheet
-chemistry <- my_data[["Chemistry"]]
-bacteria <- my_data[["Bacteria"]]
-nutrients <- my_data[["Nutrients"]]
-site_list <- my_data[["SiteList"]]
-central_coast_standards <- my_data[["CentralCoastStandards"]]
-la_basin_standards <- my_data[["LABasinStandards"]]
 
+# Remove White Space from Column Names ------------------------------------
+remove_ws_col <- function(data_frame){
+  # Custom function used to remove white space from column names
+  names(data_frame) <- gsub(" ", "", names(data_frame))
+  data_frame
+}
+
+# Map function across list of tibbles
+my_data <- my_data %>%
+  purrr::map(remove_ws_col)
+
+# Lower Case Character Columns --------------------------------------------
+lower_case_str <- function(data_frame){
+  data_frame %>%
+    # Lower caps everything if column is character type
+    dplyr::mutate_if(is.character, tolower)  
+}
+
+# Map function across list of tibbles
+my_data <- my_data %>%
+  purrr::map(lower_case_str)
+
+# Time Column Conversion --------------------------------------------------
+time_col_conversion <- function(data_frame){
+  # Custom function that searches columns which include "Time" (based on 
+  # structure of data, all columns with time are time formatted). 
+  my_vals <- colnames(data_frame) %>%
+    stringr::str_detect("Time")
+  my_names <- colnames(data_frame[my_vals])
+  
+  data_frame %>%
+    # Convert to time as character 
+    dplyr::mutate_if(names(.) %in% my_names, 
+                     dplyr::funs(as.character(strftime(., format = "%H:%M:%S", tz = 'UTC'))))
+}
+
+# Map function across list of tibbles
+my_data <- my_data %>% 
+  purrr::map(~ time_col_conversion(.x))
+
+# Date Column Conversion --------------------------------------------------
+date_col_conversion <- function(data_frame){
+  # Custom function that searches columns which include "Date" (based on 
+  # structure of data, all columns with date are date formatted). 
+  my_vals <- colnames(data_frame) %>%
+    stringr::str_detect("Date")
+  my_names <- colnames(data_frame[my_vals])
+  
+  data_frame %>%
+    # Convert to date
+    dplyr::mutate_if(names(.) %in% my_names, 
+                     dplyr::funs(lubridate::as_date(.)))
+}
+
+# Map function across list of tibbles
+my_data <- my_data %>% 
+  purrr::map(~ date_col_conversion(.x))
+
+# Manual Data Cleaning
 # Chemistry ---------------------------------------------------------------
-
-# Data cleaning
-chemistry <- chemistry %>%
-  # Lower caps everything 
-  dplyr::mutate(TestMaterial = tolower(TestMaterial)) %>%
-  dplyr::mutate(ParameterCode = tolower(ParameterCode)) %>%
-  dplyr::mutate(SampleType = tolower(SampleType)) %>%
-  dplyr::mutate(Units = tolower(Units)) %>%
-  dplyr::mutate(TeamNumber = tolower(TeamNumber)) %>%
+my_data[["Chemistry"]] <- my_data[["Chemistry"]] %>%
   # Remove character string representing NA
-  dplyr::mutate(TeamNumber = replace(TeamNumber, TeamNumber == 'n/a', NA)) %>%
-  # Convert SampleCollectionTime to datetime format
-  dplyr::mutate(SampleCollectionTime = lubridate::as_datetime(SampleCollectionTime)) %>%
-  # Convert SampleDate to date format
-  dplyr::mutate(SampleDate = lubridate::as_date(SampleDate))
+  dplyr::mutate(TeamNumber = replace(TeamNumber, TeamNumber == 'n/a', NA)) 
 
 # Bacteria ----------------------------------------------------------------
 
 # Data Cleaning 
-bacteria <- bacteria %>%
-  # Lower caps everything
-  dplyr::mutate(TestMaterial = tolower(TestMaterial)) %>%
-  dplyr::mutate(SampleType = tolower(SampleType)) %>%
+my_data[["Bacteria"]] <- my_data[["Bacteria"]] %>%
   # Replace misspelling of word
-  dplyr::mutate(ParameterCode = replace(ParameterCode, ParameterCode == "E. Coil", "E. Coli")) %>%
-  # Change format for time variables
-  dplyr::mutate(SampleCollectionTime = strftime(SampleCollectionTime, format = "%H:%M:%S", tz = 'UTC')) %>%
-  dplyr::mutate(IncubatorEntryTime = strftime(IncubatorEntryTime, format = "%H:%M:%S", tz = 'UTC')) %>%
-  dplyr::mutate(ResultReadTime = strftime(ResultReadTime, format = "%H:%M:%S", tz = 'UTC'))
+  dplyr::mutate(ParameterCode = replace(ParameterCode, ParameterCode == "e. coil", "e. coli")) 
+
 
 # Nutrients ---------------------------------------------------------------
 
-# Remove whitespace from column names to make more readable
-names(nutrients) <- gsub(" ", "", names(nutrients))
+# No manual cleaning required but if you wanted to combine date and time columns 
+# Here's how you would do it:
 
-# Data Cleaning 
-nutrients <- nutrients %>%
-  # Lower caps everything
-  dplyr::mutate(SampleDate = lubridate::as_date(SampleDate)) %>%
-  dplyr::mutate(SampleTime = strftime(SampleTime, format = "%H:%M:%S", tz = 'UTC'))
-
-
-# In case there would be benefits to create datetime columns from date and time columns here's how to do so 
-# nutrients %>%
+# my_data[["Nutrients"]] %>%
   # tidyr::unite(col = "CollectionDatetime", c("SampleDate", "SampleTime"), sep = " ")
   # OR: dplyr::mutate(SampleCol = paste(SampleDate, SampleTime))
 
 # Site List ---------------------------------------------------------------
 
-# Remove whitespace from column names to make more readable
-names(site_list) <- gsub(" ", "", names(site_list))
-
 # Data Cleaning 
-site_list <- site_list %>%
-  # Lower caps
-  dplyr::mutate(SamplingFrequency = tolower(SamplingFrequency)) %>%
+my_data[["SiteList"]] <- my_data[["SiteList"]] %>%
   # Separate Column containing Lat and Long to two columns
   tidyr::separate(`GeographicCoordinates(DecimalDegrees)`, c("Longitude", "Latitude"), sep = ",")
 
 
-# Clean Standard Column Names ---------------------------------------------
-
-names(central_coast_standards) <- gsub(" ", "", names(central_coast_standards))
-names(la_basin_standards) <- gsub(" ", "", names(la_basin_standards))
-
 # Output CSV Files --------------------------------------------------------
-clean_data = list("Chemistry" = chemistry, 
-                  "Bacteria" = bacteria, 
-                  "Nutrients" = nutrients, 
-                  "SiteList" = site_list, 
-                  "CentralCoastStandards" = central_coast_standards, 
-                  "LABasinStandards" = la_basin_standards)
 
 # Create array containing output file name and path
 csv_names <- sprintf("data/%s.csv", names(my_data))
@@ -130,8 +142,8 @@ csv_names <- sprintf("data/%s.csv", names(my_data))
 # data directory. If not then the for-loop will run and output csv files. 
 if (any(!file.exists(csv_names))) {
   # for-loop to create new csv files with tidy data
-  for (i in 1:length(clean_data)){
-    clean_data[[i]] %>%
+  for (i in 1:length(my_data)){
+    my_data[[i]] %>%
       readr::write_csv(path = csv_names[[i]])
   }
   print("Done")
